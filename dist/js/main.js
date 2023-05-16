34f528687c52ed3e4035160feef6369e7502c18b
JADundee <Jadundee@icloud.com>
2023-05-16 07:40:43 -0600

    Imported game JS and finished task layout

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,5 +1,14 @@
-const initApp = () => {
+import GameObj from "./Game";
+const Game = new GameObj();
 
+const initApp = () => {
+    // all time data
+    // update svoreboard
+    // listen for player choice
+    // listen for enter key
+    // listen for play again choice
+    // lock in gameboard height
+    // set focus to start new game
 }
 
 document.addEventListener("DOMContentLoaded", initApp);
